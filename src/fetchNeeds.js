c769b3135423426c900d27cb08cc512807d630bb
Alastair Taft <ali.c.taft@gmail.com>
2016-06-09 00:33:33 +1000

    Added support for passing in multiple parameters on the server side.

--- src/fetchNeeds.js
+++ src/fetchNeeds.js
@@ -2,13 +2,14 @@
 export default function fetchNeeds(params, store) {
 
   var components = params.components
+  var args = Array.prototype.slice.call(arguments)
 
   var needs = []
   components.forEach(c => needs.push(c.needs || null), []);
 
   const promises = needs.map((need) => {
     if (!need) return Promise.resolve(need)
-    return need(params, store)
+    return need.apply(this, args)
   });
 
   return Promise.all(promises)
